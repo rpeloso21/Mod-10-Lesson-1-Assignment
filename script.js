dce44998bb0d726ac44f48b6b05b3713fedc1402
rpeloso21 <rpeloso21@gmail.com>
2024-09-09 16:37:09 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+//Understanding Data Types in JavaScript
+
+// Task 2: Assign sample values
+let studentName = "Alice";
+let studentAge = 15;
+let studentGrade = 10;
+let isPresent = true;
+
+// Task 3: Log statements to console
+console.log(studentName)
+console.log(studentAge)
+console.log(studentGrade)
+console.log(isPresent)
+
+
+
+//Exploring JavaScript Operators
+
+
+let x = 10;
+let y = 15;
+
+let sum = x+y;
+let difference = x-y;
+let product = x*y;
+let Quotient = x/y;
+
+// Task 3: Perform arithmetic operations
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", Quotient);
+
+// Task 4: Explore assignment operators
+let num1 = 10
+let num2 = 15
+
+num1 = 21
+num2 = 25
+
+console.log("Updated num1:", num1);
+console.log("Updated num2:", num2);
+
+// Task 5: Use comparison operators
+let equal = (num1 == num2)
+let greaterThan = num1 > num2
+let notEqual = (num1 != num2)
+
+
+console.log("Is num1 equal to num2?", equal);
+console.log("Is num1 greater than num2?", greaterThan);
+console.log("Is num1 not equal to num2?", notEqual);
+
+// Task 6: Apply logical operators
+let isPositive = (num1 > 0 && num2 > 0);
+let isEven = ((num1 % 2 == 0) || (num2 % 2 == 0));
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
